ad53b5dbdc1f688df8ea7a70e22e8b8a8048954b
Artem Ugryumov <artiom.ygryumov@gmail.com>
2023-05-17 22:51:32 +0300

    UPDATE: добавить лог о пустых приходящих данных

--- api/helpers/Logger.js
+++ api/helpers/Logger.js
@@ -11,14 +11,17 @@ class Logger {
 	sendReqData() {
 		let data = {}
 
-		if (Object.keys(this.req.query).length !== 0) {
+		if (this?.req?.query && Object.keys(this.req.query).length !== 0) {
 			data.query = this.req.query
 		}
-		if (Object.keys(this.req.body).length !== 0) {
+		if (this?.req?.body && Object.keys(this.req.body).length !== 0) {
 			data.body = this.req.body
 		}
 		if (Object.keys(data).length !== 0) {
 			console.log("Приходящие данные", JSON.stringify(data))
+		}
+		else {
+			console.log("Приходящих данных нет")
 		}
 	}
 
@@ -29,7 +32,6 @@ class Logger {
 	}
 
 	sendError(status, text, err) {
-
 		console.warn(text, err ? err : '')
 		this.sendReqData()
 		this.res.status(status).send(text)
