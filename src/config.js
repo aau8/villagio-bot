994e248918877d21a4a14a39081225bce3cf7b8d
Artem Ugryumov <artiom.ygryumov@gmail.com>
2023-06-20 22:48:17 +0300

    ADD: добавить id Влада для доступа в админку

--- src/config.js
+++ src/config.js
@@ -3,6 +3,7 @@ export const ADMINS_ID = [
 	428579289,
 	803056459, // Артем (разработчик бота)
 	504764144, // Роман
+	712067885, // Влад
 ]
 export const CATALOG_URL = "https://zagorie.ru/upload/iblock/4ea/4eae10bf98dde4f7356ebef161d365d5.pdf"
 export const CONSULT_APPLIC_URL = "https://stage-forms.villagio-realty.ru/post-lead"
